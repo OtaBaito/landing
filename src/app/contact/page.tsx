ccf9068807000aa92d2c084a9c872a3e1de14ede
Fray Dalton <fray117@exploiter.id>
2023-12-29 17:42:16 +0700

    add contact page

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,10 @@
+import Mascot from '@/assets/images/josyifa/lily_sketch.jpeg'
+import Image from 'next/image'
+
+export default function Page() {
+	return (
+		<div className="flex max-w-7xl mx-auto">
+			otaku@otabyte.net
+		</div>
+	)
+}
