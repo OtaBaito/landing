01bfa78ea4e7aaa67006756973cd0013b2a9f810
Fray Dalton <fray117@exploiter.id>
2023-12-29 17:41:42 +0700

    add about page

---
+++ src/app/about/page.tsx
@@ -0,0 +1,127 @@
+export default function Page() {
+	return (
+		<div className="relative">
+			<div className="z-10 w-full max-w-xl px-5 xl:px-0">
+				<h1
+					className="mb-12 animate-fade-up bg-gradient-to-br from-black to-stone-500 bg-clip-text text-center font-display text-5xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm [text-wrap:balance] md:mb-[80px] md:text-left md:text-7xl md:leading-[5rem]"
+					style={{ animationDelay: "0.15s", animationFillMode: "forwards" }}
+				>
+					Just a brief <br />
+					of us
+				</h1>
+
+				<section className="mt-8 md:mt-10">
+					<div
+						className="flex animate-fade-up items-center gap-3 opacity-0 md:text-2xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						<div className="flex items-center gap-3 font-semibold after:mt-2 after:h-[2px] after:w-[40px] after:content-normal after:bg-black">
+							From Minus
+						</div>
+						<div>for a plus</div>
+					</div>
+
+					<p
+						className="mt-6 animate-fade-up text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						We’re had single purpose in mind, which is which for those our one
+						of kind to be entitled as “Otaku” to not rated as just a mere weebs.
+						We withstand any circumstance that might bring disgrace for our
+						honor, we tried so hard to make sure people are beneficial from our
+						community, not just for us the otaku but for everyone that are
+						hobbyist.
+					</p>
+
+					<p
+						className="mt-6 animate-fade-up text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						We are not otaku community, we are the otaku itself. We made hobby
+						to creativity as a tool or we could proudly saying it as a motto
+						that people could be see us one of great community for pouring their
+						potential as hobbyist.
+					</p>
+				</section>
+
+				<section className="mt-8 md:mt-10">
+					<div
+						className="animate-fade-up font-semibold opacity-0 md:text-2xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						Why &amp; What
+					</div>
+
+					<p
+						className="mt-6 animate-fade-up text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						As <b>Armiko</b> and <b>Zakir</b> had been discussed how great if
+						they had own virtual artist agency from seeing many popular vtuber
+						and also listen to their hits music, it’s just in early December
+						2023 when they had online discussion with the <b>Being X</b> for an
+						actual proposal as highest executive on OtaByte.
+					</p>
+
+					<p
+						className="mt-6 animate-fade-up text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						OtaByte itself was like a joke while it already launched Ohayou Post
+						even they barely maintained since OtaByte was never making profit in
+						the first place. X was aware of this situation since many “project”
+						are debunked and closed early before make any more lost to the
+						SABAKO as the parent company that subsidized everything, yes OtaByte
+						director board were also playing a role in SABAKO.
+					</p>
+
+					<p
+						className="mt-6 animate-fade-up text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						X was hesitate at first but after being violently virtual abused,
+						harassed, extorted, at last X was already pressured from the duo to
+						make a movement and already made up to actually prove this idea as
+						it called “OtaByte Project” or the X likely to call it OtaProject or
+						simply The Project.
+					</p>
+				</section>
+
+				<section className="mt-8 md:mt-10">
+					<div
+						className="animate-fade-up font-semibold opacity-0 md:text-2xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						Who &amp; When
+					</div>
+
+					<p
+						className="mt-6 animate-fade-up text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						Who you say might rather difficult to answer, but as I’m the X
+						itself written all the time, I would like to say to hunt a fresh
+						head for the talent. How many isn’t a question while I personally
+						like for quality polished rather than quantity.
+					</p>
+
+					<p
+						className="mt-6 animate-fade-up text-gray-500 opacity-0 [text-wrap:balance] md:text-xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						But pretty sure to follow the idea, I would safely assume Q2 would
+						be a great start at soonest, but I don’t know if it might be later
+						but at least hope for whatever the outcome.
+					</p>
+
+					<p
+						className="mt-6 animate-fade-up text-gray-400 opacity-0 [text-wrap:balance] md:text-xl"
+						style={{ animationDelay: "0.25s", animationFillMode: "forwards" }}
+					>
+						Frankly, the X itself
+					</p>
+				</section>
+			</div>
+		</div>
+	)
+}
